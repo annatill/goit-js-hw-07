c8d6d334938cf60ae1f36f65e784f4bac7f222eb
annatill <anna.tataurova.95@gmail.com>
2023-02-24 23:41:15 +0100

    add hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const makeImgEl = (image) => {
+  const { original, preview, description } = image;
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+
+const divEl = document.querySelector(".gallery");
+const imageMarkup = galleryItems.map(makeImgEl).join("");
+divEl.insertAdjacentHTML("beforeend", imageMarkup);
+
+divEl.addEventListener("click", onImagesClick);
+
+function onImagesClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img width="1400" height="900" src="${event.target.dataset.source}">
+`);
+  instance.show();
+
+  document.addEventListener(
+    "keydown",
+    function (event) {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const makeImgEl = (image) => {
+  const { original, preview, description } = image;
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const ulEl = document.querySelector(".gallery");
+const imageMarkup = galleryItems.map(makeImgEl).join("");
+ulEl.insertAdjacentHTML("beforeend", imageMarkup);
+
+ulEl.addEventListener("click", onImagesClick);
+
+function onImagesClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  usageSimpleLightBox();
+}
+
+function usageSimpleLightBox() {
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
